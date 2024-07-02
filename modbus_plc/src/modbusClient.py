c5124ecf95224e86d02c77680b82480db2e80f4c
Jaxson Brown <jokesene@outlook.com>
2024-07-02 23:36:41 +0800

    reworked client code

--- modbus_plc/src/modbusClient.py
+++ modbus_plc/src/modbusClient.py
@@ -8,38 +8,24 @@ import sys
 import time
 from modbusPLC import modbusTcpCom
 
-# Validate args
-if len(sys.argv) != 2:
-    print("Usage: <targetIp>")
-    print("Exitting program")
-    exit(1)
+# Function: main
+def main(args):
+    # Validate args
+    if len(args) != 2:
+        print("Usage: <targetIp>")
+        print("Exitting program")
+        exit(1)
 
-# Connect to PLC 
-hostIp = sys.argv[1]
-hostPort = 502
+    # Connect to PLC 
+    hostIp = args[1]
+    hostPort = 502
 
-# Test Modbus connection
-client = modbusTcpCom.modbusTcpClient(hostIp)
-print('\nTesting modbus server connection.')
-while not client.checkConn():
-    print('Try connect to the PLC: %s' %hostIp)
-    print(client.getCoilsBits(0, 4))
-    time.sleep(0.5)
-print('Connection successful')
-
-# Function: printMenu
-def printMenu(coilResult, hrResult, inc):
-    os.system("clear")
-    print("\n===================================================================")
-    print("Increment: %s" %inc)
-    print("Coils             (address:0, offset:4): %s" %coilResult)
-    print("Holding Registers (address:0, offset:4): %s" %hrResult)
-    print("===================================================================")
-
-# Enter testing loop
-if __name__ == "__main__":
+    # Test Modbus connection
+    client = modbusTcpCom.modbusTcpClient(hostIp)
+    testConnection(client, hostIp)
     inc = 0
 
+    # Loop the polling
     while True:
         inc += 1
         addr = 0
@@ -52,62 +38,27 @@ if __name__ == "__main__":
         printMenu(coilResult, hrResult, inc)
         time.sleep(1)
 
-"""
-#-----------------------------------------------------------------------------
-print('\nTestcase 02: read the coils: ')
-result = client.getCoilsBits(4, 5)
-correctResult = [False, False, False, False, False]
-if result == correctResult: 
-    print(" - test Pass, result: %s" %str(result))
-else:
-    print(" - test Fail, result: %s" %str(result))
-time.sleep(0.5)
 
-#-----------------------------------------------------------------------------
+# Function: testConnection
+def testConnection(client, hostIp):
+    print('\nTesting modbus server connection.')
+    while not client.checkConn():
+        print('Try connect to the PLC: %s' %hostIp)
+        print(client.getCoilsBits(0, 4))
+        time.sleep(0.5)
+    print('Connection successful')
+    return True
 
-print('\nTestCase 03: read the holding registers')
-result = client.getHoldingRegs(0, 4)
-correctResult = [1, 2, 3, 50]
-if result == correctResult: 
-    print(" - test Pass, result: %s" %str(result))
-else:
-    print(" - test Fail, result: %s" %str(result))
-time.sleep(0.5)
+# Function: printMenu
+def printMenu(coilResult, hrResult, inc):
+    os.system("clear")
+    print("\n=========================================================================")
+    print("Polling Increment: %s" %inc)
+    print("Coils             (address:0, offset:4): %s" %coilResult)
+    print("Holding Registers (address:0, offset:4): %s" %hrResult)
+    print("=========================================================================")
 
-#-----------------------------------------------------------------------------
-
-print('\nTestcase 04: Set the holding registers')
-client.setHoldingRegs(1, 20)
-time.sleep(0.5)
-result = client.getHoldingRegs(0, 4)
-correctResult = [1, 20, 3, 50]
-if result == correctResult: 
-    print(" - test Pass, result: %s" %str(result))
-else:
-    print(" - test Fail, result: %s" %str(result))
-time.sleep(0.5)
-
-#-----------------------------------------------------------------------------
-
-#print('\nTestcase 05: check auto update coils function')
-#result = client.getCoilsBits(0, 4)
-#if result == [True, False, False, False]: 
-#    print(" - test Pass, result: %s" %str(result))
-#else:
-#    print(" - test Fail, result: %s" %str(result))
-#time.sleep(0.5)
-
-#-----------------------------------------------------------------------------
-
-print('\nTestcase 06: Set the coils')
-client.setCoilsBit(1, 1)
-time.sleep(0.5)
-result = client.getCoilsBits(0, 6)
-correctResult = [False, True, False, False, False, False]
-if result == correctResult: 
-    print(" - test Pass, result: %s" %str(result))
-else:
-    print(" - test Fail, result: %s" %str(result))
-time.sleep(0.5)
-"""
-
+# Main
+if __name__ == "__main__":
+    main(sys.argv)
+    
