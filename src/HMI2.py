23ad2d87b231233853d1f3957db79ed74ded9101
Jaxson Brown <jokesene@outlook.com>
2024-07-11 16:32:59 +0800

    created solar panel simulation

---
+++ src/HMI2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import time
+import logging
+import sys
+import os
+from threading import Thread, Lock
+from pyModbusTCP.client import ModbusClient
+
+# set global variables
+holding_regs = []
+coils = []
+lock = Lock()
+
+# create logger
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+_logger.addHandler(console_handler)
+
+################################################################################
+
+"""A simple thread to simulate a HMI"""
+def hardware_in_loop(client : ModbusClient):
+    global holding_regs, coils, lock
+
+    # polling loop
+    while True:
+        coil_list = client.read_coils(10, 1)
+        reg_list = client.read_holding_registers(20, 1)
+
+        # store recorded values
+        if reg_list:
+            with lock:
+                holding_regs = list(reg_list)
+
+        if coil_list:
+            with lock:
+                coils = list(coil_list)
+
+        # delay between polls
+        time.sleep(0.5)
+
+################################################################################
+
+if __name__ == '__main__':
+    # init modbus client
+    client = ModbusClient(host="127.0.0.1", port=5020, unit_id=1)
+
+    # start the HIL client thread
+    tp = Thread(target=hardware_in_loop, args=(client,))
+    tp.daemon = True # to kill the thread when the main thread exits
+    tp.start()
+
+    _logger.info("Starting HMI Client")
+    
+    # (ASCII font "Big" https://patorjk.com/software/taag/#p=display&f=Big)
+    title = """
+        ---------------------------
+         _    _ __  __ _____ ___  
+        | |  | |  \/  |_   _|__ \ 
+        | |__| | \  / | | |    ) |
+        |  __  | |\/| | | |   / / 
+        | |  | | |  | |_| |_ / /_ 
+        |_|  |_|_|  |_|_____|____|
+        ---------------------------
+        """
+    while True:
+        # clear terminal
+        os.system('clear')
+        print(title)
+
+        with lock:
+            print("=============================================================")
+            print(f"Current reading from the solar panel power meter (mW): {holding_regs[0]}")
+            if coils[0]:
+                print("Power supply input: Solar Panels")
+            else:
+                print("Power supply input: Mains Power")
+            print("=============================================================")
+            print()
+            print()
+            print()
+            print()
+            print()
+            print()
+            print()
+            print("----------------------------")
+            print("| Raw Modbus Input:")
+            print(f"| holding registers: {holding_regs}")
+            print(f"| coils: {coils}")
+            print("----------------------------")
+
+        time.sleep(0.5)
---
+++ src/PLC1.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import logging
+import sys
+from threading import Thread, Lock
+import time
+from pyModbusTCP.server import ModbusServer, DataBank
+from pyModbusTCP.client import ModbusClient
+
+# set global variables
+#solar_panel_pm = []
+lock = Lock()
+
+# create logger
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+_logger.addHandler(console_handler)
+
+################################################################################
+
+"""
+Inherited Class: DataBankLogic
+Purpose: A custom ModbusServerDataBank to control logic within the PLC
+"""
+class DataBankLogic(DataBank):
+    """
+    Overrided Function: on_holding_registers_change
+    Purpose: Defines logic to be executed when a holding register value changes
+    """
+    def on_holding_registers_change(self, address, from_value, to_value, srv_info):
+        # control the transfer switch
+        if address == 20:
+            self.__transfer_switch_logic(to_value, 800, 10)
+
+    """
+    Private Function: transfer_switch_logic
+    Purpose: Simulates the transfer swtich logic. When the supplied milliwattage
+        from the holding register is below the given power
+        threshold, power input will switch to mains and vice versa via given coil.
+    """
+    def __transfer_switch_logic(self, power, power_thresh, switch_addr):
+        # check whether need to switch to mains (0) or keep solar (1)
+        if power < power_thresh:
+            # switch to mains
+            self.set_coils(switch_addr, [False])
+        else:
+            # switch to solar power
+            self.set_coils(switch_addr, [True])
+
+
+################################################################################
+
+"""Thread for the PLC server"""
+def plc_server(server : ModbusServer):
+    _logger.info(f"Starting PLC1 Server")
+    server.start()
+
+################################################################################
+
+"""Thread for the PLC client"""
+#def plc_client(client : ModbusClient):
+#    _logger.info(f"Starting PLC1 Client")
+#    global lock, solar_panel_pm
+
+    # polling loop
+#    while True:
+#        reg_list = client.read_holding_registers(20, 1)
+
+        # store recorded values
+#        if reg_list:
+#            with lock:
+                # store solar panel power meter readings
+#                solar_panel_pm = reg_list[0]
+
+        # delay between polls
+#        time.sleep(0.5)
+
+################################################################################
+
+if __name__ == '__main__':
+    server_ip = "127.0.0.1"
+    server_port = 5020
+
+    client_target_ip = "127.0.0.1"
+    client_target_port = 5020
+
+    # (ASCII font "Big" https://patorjk.com/software/taag/#p=display&f=Big)
+    title = """
+        -------------------------
+         _____  _      _____ __ 
+        |  __ \| |    / ____/_ |
+        | |__) | |   | |     | |
+        |  ___/| |   | |     | |
+        | |    | |___| |____ | |
+        |_|    |______\_____||_|
+        -------------------------
+        """
+    print(title)
+
+    # init modbus server
+    server = ModbusServer(host=server_ip, port=server_port, data_bank=DataBankLogic())
+
+    # init modbus client
+#    client = ModbusClient(host=client_target_ip, port=client_target_port, unit_id=1)
+
+    # start the PLC server thread
+    tp_server = Thread(target=plc_server, args=(server,))
+    tp_server.daemon = True
+    tp_server.start()
+    
+    # start the PLC client thread
+#    tp_client = Thread(target=plc_client, args=(client,))
+#    tp_client.daemon = True
+#    tp_client.start()
+
+    while True:
+        with lock:
+            print()
+        
+        time.sleep(1)
+
+
+################################################################################
